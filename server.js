2c6ec6560e9c76ad490fefa9bc1035e2770aa1ff
brenden-sullivan <bsulli13@u.rochester.edu>
2015-11-18 17:06:35 -0500

    Ignore this too

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.delete('/users/*', function(req, res) {
 
 //start server
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 http.listen(server_port, server_ip, function(){
   console.log('listening on *:3333');
