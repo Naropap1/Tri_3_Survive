ca36d594e816cb16e4eed200d965e2412af76b46
brenden-sullivan <bsulli13@u.rochester.edu>
2015-11-21 02:10:11 -0500

    Ignore This

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ app.delete('/users/*', function(req, res) {
 });
 
 //start server
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8000;
 var server_ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 http.listen(server_port, server_ip, function(){
