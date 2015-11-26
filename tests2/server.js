d7fa3086c6f1b57fd481bcbbe6fa0b166200882c
naropian <naropap1@gmail.com>
2015-11-26 17:35:17 -0500

    uploaded game progress
    resizable map multiplayer working, everything is dynamic and works off
    of a few scaler variable

---
+++ tests2/server.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var currId = 0;
+
+var players = new Array();
+
+//support parsing post request bodies
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('static'));
+
+io.on('connection', function(socket) {
+	//console.log('user connected');
+	var player = {
+		id: currId,
+		score: 0,
+		x: 100,
+		y: 100,
+		vector:0
+	};
+	var id = currId;
+	io.emit('get-id',currId);
+	currId++;
+	players.push(player);
+	//console.log(players.length);
+	io.emit('update-players',players);
+
+	socket.on('update-player', function(newX,newY,newV){
+		var index = -1;
+		for (var i = players.length - 1; i >= 0; i--) {
+			if(players[i].id == id)
+				index = i;
+		};
+		players[index].x=newX;
+		players[index].y=newY;
+		players[index].vector=newV;
+		io.emit('update-players',players)
+	});
+
+	socket.on('disconnect', function(){
+		var index = -1;
+		for (var i = players.length - 1; i >= 0; i--) {
+			if(players[i].id == id)
+				index = i;
+		};
+		players.splice(index,1);
+		io.emit('update-players',players);
+    	//console.log('user disconnected');
+    	//console.log(players.length);
+ 	 });
+
+	socket.on('chat-message', function(msg) {
+		io.emit('chat-message', msg);
+	});
+});
+
+http.listen(3333, function(){
+  console.log('listening on *:3333');
+});
